ce3e2ad2729f807938a1ef141fd97eb31b6ae923
shinseola <shinseola0528@gmail.com>
2025-09-22 09:32:37 +0900

    Initial commit

---
+++ src/main/java/iruka/gorogoro/GorogoroApplication.java
@@ -0,0 +1,13 @@
+package iruka.gorogoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorogoroApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GorogoroApplication.class, args);
+  }
+
+}
---
+++ src/test/java/iruka/gorogoro/GorogoroApplicationTests.java
@@ -0,0 +1,13 @@
+package iruka.gorogoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorogoroApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
